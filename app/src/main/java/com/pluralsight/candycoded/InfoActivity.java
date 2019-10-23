4bb15678e485697cf57137bb59e33e30ff9c5963
Mudasir-Issah <danladi496@gmail.com>
2019-10-23 11:08:49 +0100

    Reconsiliing with some resets

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -15,6 +16,8 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        findViewById(R.id.text_view_address);
 
         Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
         ImageView candyStoreImageView = (ImageView)findViewById(R.id.image_view_candy_store);
@@ -34,7 +37,7 @@ public class InfoActivity extends AppCompatActivity {
         // Create URI Below
 
         Uri geoPoints = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        // creating Intent
+        // creating Intents
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, geoPoints);
         mapIntent.setPackage("com.google.android.apps.maps");
        if (mapIntent.resolveActivity(getPackageManager()) != null){
